677dc7a1b7dbf102eb34ce15147b5447b20d5480
Tomas Torres <tomas3torres@live.com>
2020-01-14 16:00:56 -0600

    refactoring

--- main.py
+++ main.py
@@ -1,40 +1,53 @@
-delimiters = {' ','.',',','\n', ':', ';', '"', '(' ')', '{', '}', '[', ']', '?', '!'}
-
-inputFile = open('input.txt', 'r')
-paragraph = inputFile.read() + ' '
-inputFile.close()
-
-#Gets words
-words = []
-curWord = ""
-write = False
-for char in paragraph:
-    if char in delimiters and not write:
-        continue
-    if char in delimiters:
-        write = False
-        words.append(curWord)
-        curWord = ""
-    else:
-        write = True
-        curWord += char.lower()
+def getWordsFromFile(filename):
+    inputFile = open(filename, 'r')
+    paragraph = inputFile.read() + ' '
+    inputFile.close()
+    return parseParagraphForWords(paragraph)
 
 
-histogram = {}
-justify = 0
-for word in words:
-    if word in histogram:
-        histogram[word] += 1
-    else:
-        histogram[word] = 1
-        wordLength = len(word)
-        if wordLength > justify:
-            justify = wordLength
+def parseParagraphForWords(paragraph):
+    delimiters = {' ','.',',','\n', ':', ';', '"', '(' ')', '{', '}', '[', ']', '?', '!'}
 
-histogram = sorted(histogram.items(), key=lambda x: x[1], reverse=True)
-outputFile = open('output.txt', 'w')
-for item in histogram:
-    word = item[0]
-    count = item[1]
-    outputFile.write(f'{word.rjust(justify)} | {chr(35) * count} ({count})\n')
-outputFile.close()
+    words = []
+    curWord = ""
+    write = False
+    for char in paragraph:
+        if char in delimiters and not write:
+            continue
+        if char in delimiters:
+            write = False
+            words.append(curWord)
+            curWord = ""
+        else:
+            write = True
+            curWord += char.lower()
+    return words
+
+
+def main():
+    filename = "input.txt"
+    words = getWordsFromFile(filename)
+
+
+    histogram = {}
+    justify = 0
+    for word in words:
+        if word in histogram:
+            histogram[word] += 1
+        else:
+            histogram[word] = 1
+            wordLength = len(word)
+            if wordLength > justify:
+                justify = wordLength
+
+    histogram = sorted(histogram.items(), key=lambda x: x[1], reverse=True)
+    outputFile = open('output.txt', 'w')
+    for item in histogram:
+        word = item[0]
+        count = item[1]
+        outputFile.write(f'{word.rjust(justify)} | {chr(35) * count} ({count})\n')
+    outputFile.close()
+
+if __name__ == '__main__':
+    main()
+
